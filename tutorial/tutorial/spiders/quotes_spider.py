067159386f2762971461e4af5fa45fbd8b327b7a
duogglong <nduonglong02@gmail.com>
2025-05-24 17:51:28 +0700

    tut 1

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,21 +1,24 @@
-from pathlib import Path
-
 import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    async def start(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        "https://quotes.toscrape.com/page/1/",
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
+
+        yield from response.follow_all(css="ul.pager a", callback=self.parse)
+        # for href in response.css("ul.pager a::attr(href)"):
+        #     yield response.follow(href, callback=self.parse)
+        # next_page = response.css("li.next a::attr(href)").get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
